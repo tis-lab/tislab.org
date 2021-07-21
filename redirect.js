c02cdd0f7562aae7794b50293cc054899c8d97ea
Vincent Rubinetti <vince.rubinetti@gmail.com>
2021-07-21 16:28:01 -0400

    implement redirect

---
+++ redirect.js
@@ -0,0 +1,13 @@
+// on 404, redirect if path matches any entry in redirects.tsv map
+const redirect = async () => {
+  const tsv = await (await fetch("redirects.tsv")).text();
+  const rows = tsv.split("\n").map((line) => line.split("\t"));
+  const path = window.location.pathname.slice(1);
+  const match = rows.find(([from]) => from === path);
+  if (!match) return;
+  const [from, to] = match;
+  console.log(`Redirecting from ${from} to ${to}`);
+  window.history.pushState(null, null, to);
+};
+
+redirect();
