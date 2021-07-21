adfcf52355018bebb2a0192f510b62d4c009b3b9
Vincent Rubinetti <vince.rubinetti@gmail.com>
2021-07-21 17:18:03 -0400

    outsource redirects

--- redirect.js
+++ redirect.js
@@ -1,6 +1,8 @@
 // on 404, redirect if path matches any entry in redirects.tsv map
+const list =
+  "https://raw.githubusercontent.com/tis-lab/tislab.org-redirects/main/redirects.tsv?token=AB7QZOZVYIMYD7LQOG4TUQ3A7CHL4";
 const redirect = async () => {
-  const tsv = await (await fetch("redirects.tsv")).text();
+  const tsv = await (await fetch(list)).text();
   const rows = tsv.split("\n").map((line) => line.split("\t"));
   const path = window.location.pathname.slice(1);
   const match = rows.find(([from]) => from === path);
