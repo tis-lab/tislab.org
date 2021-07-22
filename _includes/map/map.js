97b77b38768645b91e21bff7e4f756181310c226
Vincent Rubinetti <vince.rubinetti@gmail.com>
2021-07-22 15:46:03 -0400

    add touch support to map!

--- _includes/map/map.js
+++ _includes/map/map.js
@@ -8,11 +8,21 @@ const resetView = () => {
   updateView();
 };
 
-const rotateView = ({ clientX, clientY }) => {
+const rotateView = (event) => {
+  let x = 0;
+  let y = 0;
+  if (event.type === "mousemove") {
+    x = event.clientX;
+    y = event.clientY;
+  }
+  if (event.type === "touchmove") {
+    x = event.touches[0].clientX;
+    y = event.touches[0].clientY;
+  }
   const { left, top, width, height } = document.body.getBoundingClientRect();
 
-  rotateX = 80 - 20 * ((clientY - top) / height);
-  rotateZ = 10 - 20 * ((clientX - left) / width);
+  rotateX = 80 - 20 * ((y - top) / height);
+  rotateZ = 10 - 20 * ((x - left) / width);
 
   updateView();
 };
@@ -33,4 +43,5 @@ const replayAnimations = () => {
 
 document.documentElement.addEventListener("mouseleave", resetView);
 document.documentElement.addEventListener("mousemove", rotateView);
+document.documentElement.addEventListener("touchmove", rotateView);
 document.documentElement.addEventListener("click", replayAnimations);
