05c43df2976636039af6cc05a8a28dc744f6fb70
Vincent Rubinetti <vince.rubinetti@gmail.com>
2021-08-11 11:51:47 -0400

    make redirect faster by including map in site

--- redirect.js
+++ redirect.js
@@ -1,15 +1,20 @@
-// on 404, redirect if path matches any entry in redirects tsv map
-const list =
-  "https://storage.googleapis.com/tislab-redirects/tislab.org-redirects.tsv";
-const redirect = async () => {
-  const tsv = await (await fetch(list)).text();
-  const rows = tsv.split("\n").map((line) => line.split("\t"));
-  const path = window.location.pathname.slice(1);
-  const match = rows.find(([from]) => from === path);
-  if (!match) return;
-  const [from, to] = match;
+// on 404, redirect if path matches any entry in redirects list
+
+const tsv =
+  "bWVsem9vbQlodHRwczovL3VjZGVudmVyLnpvb20udXMvai82NDIzMzUyNDE0P3B3ZD1UMFJDVkZFek9VWktaSEpHY2taSGNFeFBWRnBtZHowOQpqdWx6b29tCWh0dHBzOi8vdWNkZW52ZXIuem9vbS51cy9qLzI2NjI3NDUxODI/cHdkPVV6VTFWMFp4YlZaWVdHMTZhaTlIVDBKSU9EZFdaejA5Cm5pY29sZXpvb20JaHR0cHM6Ly91Y2RlbnZlci56b29tLnVzL3MvMjI5NzA2Nzc1OD9wd2Q9U25GeU1raEpXR1pYWlZCb1FXcDRVVUU1T1hneFFUMDk=";
+
+const map = Object.fromEntries(
+  window
+    .atob(tsv)
+    .split("\n")
+    .map((line) => line.split(/\s/).filter((column) => column))
+    .filter((line) => line)
+);
+
+const from = window.location.pathname.slice(1);
+const to = map[from];
+
+if (from && to) {
   console.log(`Redirecting from ${from} to ${to}`);
   window.location.href = to;
-};
-
-redirect();
+}
